a44c8409e630bff98aa6713170d03927c51c406f
farhan <farhansadikgalib@gmail.com>
2021-07-04 20:31:23 +0600

    App Icon, Splash Screen is completed.Also webview plugin is implemented.

---
+++ lib/Home_Page/HomePage.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+     return  Scaffold(
+    //   url: "https://carryforward.bizzware.net/",
+    //   withJavascript: true,
+    //
+    //   withZoom: false,
+    //   appBar: AppBar(
+        // title: Text("NU Portal"),
+        // centerTitle: true,
+   //   ),
+   );
+  }
+
+
+
+
+}
---
+++ lib/Splash_Screen/Splash Screen.dart
@@ -0,0 +1,115 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:liquid_progress_indicator_ns/liquid_progress_indicator.dart';
+import 'package:wealth_management/Home_Page/HomePage.dart';
+
+class SplashScreenPage extends StatefulWidget {
+  const SplashScreenPage({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenPageState createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 5),
+        () => Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => HomePage()),
+            (route) => false));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(1, 60, 88, 1),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Container(
+                height: 150,
+                width: 150,
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Image.asset(
+                    "assets/logo.png",
+                  ),
+                )),
+          ),
+          SizedBox(
+            height: 100,
+          ),
+          Text("CARRY FORWARD",
+              style: TextStyle(fontSize: 14, fontFamily: "Poppins",color: Colors.white60)),
+          SizedBox(
+            height: 10,
+          ),
+          _AnimatedLiquidLinearProgressIndicator(),
+        ],
+      ),
+    );
+  }
+}
+
+class _AnimatedLiquidLinearProgressIndicator extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() =>
+      _AnimatedLiquidLinearProgressIndicatorState();
+}
+
+class _AnimatedLiquidLinearProgressIndicatorState
+    extends State<_AnimatedLiquidLinearProgressIndicator>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 5),
+    );
+
+    _animationController.addListener(() => setState(() {}));
+    _animationController.repeat();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final percentage = _animationController.value * 100;
+    return Center(
+      child: Container(
+        width: 200,
+        height: 15.0,
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: LiquidLinearProgressIndicator(
+          value: _animationController.value,
+          backgroundColor: Colors.white,
+          valueColor: AlwaysStoppedAnimation(Colors.blueAccent),
+          borderRadius: 5.0,
+          borderColor: Colors.black,
+          borderWidth: 2.0,
+          // center: Text(
+          //   "${percentage.toStringAsFixed(0)}%",
+          //   style: TextStyle(
+          //     color: Colors.lightBlueAccent,
+          //     fontSize: 20.0,
+          //     fontWeight: FontWeight.bold,
+          //   ),
+          // ),
+        ),
+      ),
+    );
+  }
+}
